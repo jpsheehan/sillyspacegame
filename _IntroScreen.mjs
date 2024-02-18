901534b0532894f52bb4215ea3bd3d589b62e6d3
Jesse Sheehan <jesse.sheehan@phocassoftware.com>
2024-02-19 12:02:01 +1300

    Add instructions

--- _IntroScreen.mjs
+++ _IntroScreen.mjs
@@ -23,7 +23,14 @@ export class IntroScreen extends State {
 
                 starfield.render(ctx, time)
 
-                drawTextCentered(ctx, "Press Space to Start", CanvasSize.w / 2, CanvasSize.h / 2, "#ffffff", "bold 48px sans-serif");
+                const center = CanvasSize.w / 2;
+                const middle = CanvasSize.h / 2;
+
+                drawTextCentered(ctx, "Silly Space Game", center, middle - 150, "#ffffff", "bold 72px sans-serif");
+                drawTextCentered(ctx, "W: Accelerate", center, middle - 50, undefined, "bold 48px sans-serif");
+                drawTextCentered(ctx, "A, D: Rotate", center, middle);
+                drawTextCentered(ctx, "Fly through the green gates", center, middle + 100);
+                drawTextCentered(ctx, "Press Space to Start", center, middle + 200, "#ffffff", "bold 48px sans-serif");
             });
     }
 }
